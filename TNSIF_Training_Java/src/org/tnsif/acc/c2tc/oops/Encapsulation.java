c9c6484bae0b3c57327db2c947ef19ec88a7474a
chand <chand@LAPTOP-0H4VOS50>
2026-07-23 14:28:16 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/oops/Encapsulation.java
@@ -0,0 +1,32 @@
+package org.tnsif.acc.c2tc.oops;
+
+
+
+class Human{
+	private String name;
+	private int age;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+public class Encapsulation {
+	public static void main(String[] args) {
+		 Human obj= new Human();
+		 obj.setName("Chandu");
+		 obj.setAge(21);
+		 System.out.println(obj.getName());
+		 System.out.println(obj.getAge());
+		
+	}
+}
+}
+
